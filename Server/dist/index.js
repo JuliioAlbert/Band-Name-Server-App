e10ae02a8616aefc311b541925f53a920b87d473
Julio Gonzalez <juliioalbert98@gmail.com>
2021-10-06 00:33:42 -0500

    Socket-Server Iniciado

--- Server/dist/index.js
+++ Server/dist/index.js
@@ -3,9 +3,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var app = (0, express_1.default)();
-var port = 3000;
-app.listen(port, function () {
-    console.log("Corriendo en el puerto " + port);
+const express_1 = __importDefault(require("express"));
+const http_1 = require("http");
+const path_1 = __importDefault(require("path"));
+const socket_io_1 = require("socket.io");
+const app = (0, express_1.default)();
+const httpServer = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(httpServer, { /* options */});
+require('dotenv').config();
+///Sockets
+io.on("connect", (socket) => {
+    console.log("Cliente Conectado ");
+    socket.on('disconnect', () => {
+        console.log("Cliente Desconnectado");
+    });
+});
+//Path Publica
+const publicPath = path_1.default.resolve(__dirname, 'public');
+app.use(express_1.default.static(publicPath));
+httpServer.listen(process.env.PORT, () => {
+    console.log(`Corriendo en el puerto ${process.env.PORT}`);
 });
--- Server/index.ts
+++ Server/index.ts
@@ -1,13 +1,40 @@
-import express from 'express';
+import express from "express";
+import { createServer } from "http";
+import path from 'path';
 
+import { Server } from "socket.io";
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
 
-const port = 3000;
+
+require('dotenv').config();
 
 
-app.listen(port, () => {
-    console.log(`Corriendo en el puerto ${port}`);
+///Sockets
+
+
+io.on("connect", (socket) => {
+   console.log("Cliente Conectado ");
+
+   socket.on('disconnect', () => { 
+    console.log("Cliente Desconnectado");
+   });
+  });
+
+
+
+//Path Publica
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+
+
+
+
+httpServer.listen(process.env.PORT, () => {
+    console.log(`Corriendo en el puerto ${process.env.PORT}`);
 });
 
 
